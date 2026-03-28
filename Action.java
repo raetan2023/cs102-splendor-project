e043113eb336dad315d2386b160cc4ee7182cb8d
raetan2023 <tanszelui@gmail.com>
2026-03-28 10:51:06 +0800

    add Action.java, PurchaseCard.java, ReserveCard.java, TakeGems.java

--- Action.java
+++
@@ -1,8 +0,0 @@
-package com.splendor.core;
-public abstract class Action {
-    
-    public abstract boolean isValid(Player player, Board board);
-    
-    public abstract void takeAction(Player player, Board board);
-    
-}
--- src/com/splendor/core/ReserveCard.java
+++ src/com/splendor/core/ReserveCard.java
@@ -1,4 +1,33 @@
+/**
+ * assumptions made outside of UML:
+ * Player has getWallet(): PlayerAssets
+ * Player has getReservedCards(): List<DevelopmentCard>
+ */
+
 package com.splendor.core;
 
-public class ReserveCard {
+import com.splendor.player.Player;
+import com.splendor.model.DevelopmentCard;
+
+public class ReserveCard extends Action {
+    private DevelopmentCard card;
+
+    public ReserveCard(DevelopmentCard card) {
+        this.card = card;
+    }
+
+    @Override
+    public boolean isValid(Player player, Board board) {
+        return player.getReservedCards().size() < 3;
+    }
+
+    @Override
+    public void takeAction(Player player, Board board) {
+        player.reserve(card);
+        
+        if (board.getGoldSupply() > 0) { 
+            player.getWallet().addToken(5, 1);
+            board.setGoldSupply(board.getGoldSupply() - 1);
+        }
+    }
 }
