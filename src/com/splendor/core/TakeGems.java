63d89fede02268ae98566ff35784f11ee399d15b
anjals2804 <anjali.s.2025@computing.smu.edu.sg>
2026-04-01 20:42:27 +0800

    Updated with final comments

--- src/com/splendor/core/TakeGems.java
+++ src/com/splendor/core/TakeGems.java
@@ -1,11 +1,3 @@
-/**
- * assumptions made outside of UML:
- * Player has getWallet(): PlayerAssets
- * PlayerAssets has getTokens(): int[]
- * PlayerAssets has setTokens(int[])
- * GameView has promptDiscard(Player, int): int[]
- */
-
 package com.splendor.core;
 
 import com.splendor.player.Player;
@@ -26,6 +18,7 @@ public class TakeGems extends Action {
         boolean takingTwoSame = false;
         GemPile[] gemBank = board.getGemBank();
 
+        //iterates through count of each colour player wants to take
         for (int i = 0; i < gemsToTake.length; i++) {
             int count = gemsToTake[i];
             if (count > 0) {
@@ -35,6 +28,7 @@ public class TakeGems extends Action {
                 totalTaken += count;
                 colorsTaken++;
 
+                //checks if there are enough gems on the board depending on if player wants to take two of the same colour or only one
                 if (count == 2) {
                     takingTwoSame = true;
                     if (!gemBank[i].canTakeTwo()) {
@@ -50,6 +44,9 @@ public class TakeGems extends Action {
             }
         }
 
+        /*returns if action is valid:
+            - if two of the same colors are taken, verifies that two tokens are taken from only one color
+            - otherwise, confirms that 1-3 tokens are taken, all of different colours */
         if (takingTwoSame) {
             return totalTaken == 2 && colorsTaken == 1;
         } else {
@@ -62,6 +59,7 @@ public class TakeGems extends Action {
         PlayerAssets wallet = player.getWallet();
         GemPile[] gemBank = board.getGemBank();
 
+        //adds chosen gems first before checking if total gems are above limit (10)
         for (int i = 0; i < gemsToTake.length; i++) {
             if (gemsToTake[i] > 0) {
                 wallet.addToken(i, gemsToTake[i]);
@@ -69,26 +67,34 @@ public class TakeGems extends Action {
             }
         }
 
-        // Discard logic via GameView prompt for CLI
+        // checks if token limit is reached and discards tokens using GameView, promptDiscard()
         if (wallet.aboveTenTokens()) {
             int overLimit = 0;
+            //currentTokens is number of gems of each color that player currently has
             int[] currentTokens = wallet.getTokens();
+
+            //finds how many tokens of each color need to be discarded
             for (int count : currentTokens) {
                 overLimit += count;
             }
             overLimit -= 10;
 
-            // Using GameView to block execution and get CLI input from the player
+            // uses GameView to block execution and get CLI input from the player
             int[] discarded = GameView.promptDiscard(player, overLimit);
 
             for (int i = 0; i < currentTokens.length; i++) {
+                //subtracts number of discarded tokens of each color from player's current tokens
                 currentTokens[i] -= discarded[i];
+
+                //returns discarded tokens to the board
                 if (i == 5) {
                     board.returnGold(discarded[i]);
                 } else {
                     gemBank[i].returnGems(discarded[i]);
                 }
             }
+            
+            //updates player's wallet with to remove discarded tokens 
             wallet.setTokens(currentTokens);
         }
     }
