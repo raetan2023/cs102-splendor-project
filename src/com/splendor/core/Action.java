5af410e3ed19c9930b471a13f3fa3ec8915f3d06
anjals2804 <anjali.s.2025@computing.smu.edu.sg>
2026-04-01 20:26:27 +0800

    Updated with final comments

--- src/com/splendor/core/Action.java
+++ src/com/splendor/core/Action.java
@@ -1,11 +1,11 @@
-//abstract interface to create structure for all player moves
+// abstract interface to create structure for all player moves
 package com.splendor.core;
 
 import com.splendor.player.Player;
 
 public abstract class Action {
-    //checks if selected action is valid for given player
+    // checks if selected action is valid for given player
     public abstract boolean isValid(Player player, Board board);
-    //executes action
+    // executes action
     public abstract void takeAction(Player player, Board board);
 }
