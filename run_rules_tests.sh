2942eb1878543bb08beae7ed919fd6bc795770eb
anjals2804 <anjali.s.2025@computing.smu.edu.sg>
2026-04-01 19:20:35 +0800

    Update run_rules_tests.sh

--- run_rules_tests.sh
+++ run_rules_tests.sh
@@ -1,22 +1,9 @@
-#!/bin/bash
-# Script to compile and run RulesTestCases
-
 # Move to the directory where this script is located
 cd "$(dirname "$0")"
 
-echo "====================================="
-echo " Rules Test Cases Runner"
-echo "====================================="
-echo ""
-
-# Compile the project
-echo "Compiling Splendor project..."
-./compile.sh
-
-if [ $? -ne 0 ]; then
-    echo ""
-    echo "Compilation failed. Exiting."
-    exit 1
+echo "Running RulesTestCases..."
+java -cp classes com.splendor.RulesTestCases
+1
 fi
 
 echo ""
