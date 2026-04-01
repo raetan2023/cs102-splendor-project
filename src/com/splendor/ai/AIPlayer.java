c2fdd7dc63e6f3190c1378dffb3aeb7aaae3d75e
anjals2804 <anjali.s.2025@computing.smu.edu.sg>
2026-04-01 19:02:32 +0800

    Update AIPlayer.java

--- src/com/splendor/ai/AIPlayer.java
+++ src/com/splendor/ai/AIPlayer.java
@@ -1,7 +1,17 @@
 package com.splendor.ai;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import com.splendor.core.Action;
 import com.splendor.core.Board;
+import com.splendor.core.PurchaseCard;
+import com.splendor.core.ReserveCard;
+import com.splendor.core.TakeGems;
+import com.splendor.model.DevelopmentCard;
+import com.splendor.model.GemColor;
 import com.splendor.player.Player;
 
 public class AIPlayer extends Player {
@@ -18,7 +28,56 @@ public class AIPlayer extends Player {
     }
 
     public Action chooseAction(Board board) {
-        return strategy.chooseAction(this, board);
+        List<DevelopmentCard> availableCards = getAllVisibleCards(board);
+        Map<GemColor, Integer> availableGems = getAvailableGems(board);
+        
+        Decision decision = strategy.chooseAction(this, availableCards, availableGems);
+        return decisionToAction(board, decision);
+    }
+
+    private List<DevelopmentCard> getAllVisibleCards(Board board) {
+        List<DevelopmentCard> visibleCards = new ArrayList<>();
+        for (List<DevelopmentCard> tierCards : board.getVisibleCards().values()) {
+            visibleCards.addAll(tierCards);
+        }
+        return visibleCards;
+    }
+
+    private Map<GemColor, Integer> getAvailableGems(Board board) {
+        Map<GemColor, Integer> availableGems = new HashMap<>();
+        for (int i = 0; i < 5; i++) {
+            GemColor color = GemColor.values()[i];
+            availableGems.put(color, board.getGemBank()[i].getSupply());
+        }
+        return availableGems;
+    }
+
+    private Action decisionToAction(Board board, Decision decision) {
+        if (decision == null) {
+            return null;
+        }
+
+        switch (decision.getType()) {
+            case PURCHASE:
+                DevelopmentCard card = decision.getCard();
+                boolean isReserved = this.getReservedCards().contains(card);
+                return new PurchaseCard(card, isReserved);
+            case RESERVE:
+                return new ReserveCard(decision.getCard());
+            case TAKE_GEMS:
+                return gemsListToAction(decision.getGemColors());
+            case PASS:
+            default:
+                return null;
+        }
+    }
+
+    private Action gemsListToAction(List<GemColor> gemColors) {
+        int[] gemsToTake = new int[5];
+        for (GemColor color : gemColors) {
+            gemsToTake[color.ordinal()]++;
+        }
+        return new TakeGems(gemsToTake);
     }
 
     public Strategy getStrategy() {
