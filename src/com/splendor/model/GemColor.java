4052fa23f04ac1f23d5ab6cbfcdf3ce0caf39149
darrenteo17 <darrenteojunli@gmail.com>
2026-04-02 21:20:58 +0800

    comments for GemColor.java
    Added comment to clarify the purpose of the GOLD color.

--- src/com/splendor/model/GemColor.java
+++ src/com/splendor/model/GemColor.java
@@ -1,10 +1,11 @@
 package com.splendor.model;
 
+// public enumeration used which is a special class that represents a fixed set of constants to hold our Gem colors.
 public enum  GemColor {
     WHITE,
     BLUE,
     GREEN,
     RED,
     BLACK,
-    GOLD
+    GOLD // Joker/wildcard
 }
