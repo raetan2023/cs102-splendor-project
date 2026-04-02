acb07d1729ae5e9c4c4e35e5dbd1d4f6d8d5e778
darrenteo17 <darrenteojunli@gmail.com>
2026-04-02 21:35:29 +0800

    comments for Noble.java
    Added comments to class variables and methods for better clarity.

--- src/com/splendor/model/Noble.java
+++ src/com/splendor/model/Noble.java
@@ -5,11 +5,12 @@ import java.util.List;
 import com.splendor.player.Player;
 
 public class Noble {
-    private String name;
-    private List<GemColor> requirementColors;
-    private List<Integer> requirementQty;
-    private int points;
+    private String name; // Name of nobel
+    private List<GemColor> requirementColors; // List of specific gems required to get noble
+    private List<Integer> requirementQty; // List of number of each gem required to get noble
+    private int points; // prestige points the noble awards
 
+    // Constructor to initialize the noble
     public Noble(String name, List<GemColor> requirementColors, List<Integer> requirementQty, int points) {
         this.name = name;
         this.requirementColors = requirementColors;
@@ -17,26 +18,32 @@ public class Noble {
         this.points = points;
     }
 
+    // Getter for name
     public String getName() {
         return name;
     }
 
+    // Getter for required colors
     public List<GemColor> getRequirementColors() {
         return requirementColors;
     }
 
+    // Getter for required number of each colored gem
     public List<Integer> getRequirementQty() {
         return requirementQty;
     }
 
+    // Getter for number of prestige points the noble gives
     public int getPoints() {
         return points;
     }
 
+    // Checks if the Player meets the requirements for this noble
     public boolean needs(Player p) {
         return p.canAfford(this);
     }
 
+    // Override toString method to print out noble in the format "Noble [Name: xxx | Points: x | Requirements: x] "
     @Override
     public String toString() {
         return "Noble [Name:" + name + 
@@ -44,6 +51,7 @@ public class Noble {
         " | Requirements: " + formatRequirements() + "]";
     }
 
+    // Helper method for toString to print out the requirements in the format "[GemColor = x]"
     private String formatRequirements() {
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < requirementColors.size(); i++) {
